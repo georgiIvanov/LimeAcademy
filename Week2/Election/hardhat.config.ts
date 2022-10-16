d5551dbfcfad62f891578653b123b0a124bd1b5d
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2022-10-16 10:42:31 +0300

    W2 - USElection contract

--- Week2/Election/hardhat.config.ts
+++ Week2/Election/hardhat.config.ts
@@ -5,4 +5,16 @@ const config: HardhatUserConfig = {
   solidity: "0.8.17",
 };
 
+module.exports = {
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
+
 export default config;
