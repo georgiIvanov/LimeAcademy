36b2b2629979e245f31b1977316b86cbd67b6db1
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2022-10-23 23:39:07 +0300

    W2 - Deploy function

--- Week2/BookLibrary/scripts/deploy.ts
+++ Week2/BookLibrary/scripts/deploy.ts
@@ -1,12 +1,16 @@
+import { HardhatEthersHelpers, HardhatRuntimeEnvironment } from "hardhat/types";
 import { ethers } from "hardhat";
 
-async function main() {
-  
+export const deployContract = async (ethers: HardhatEthersHelpers) => {
+  const bookLibraryFactory = await ethers.getContractFactory("BookLibrary");
+  const bookLibrary = await bookLibraryFactory.deploy();  
+  console.log("BookLibrary contract deployed to:", bookLibrary.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
+// Deploys contract to a network
+// Example: npx hardhat run --network localhost scripts/deploy.ts
+// you can replace `localhost` with any network
+deployContract(ethers).catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
