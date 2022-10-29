0eefed2873c4855625910cc15eb66dc984498214
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2022-10-30 00:22:26 +0300

    W3 - create wallet with ethers

--- Week3/Election/README.md
+++ Week3/Election/README.md
@@ -2,5 +2,15 @@
 
 This project builds on what we've learned in the past three weeks.
 
-We're using it to explore how to work with ethers.js.
+We're using it to explore how to work with ethers.js
+- Connect to various networks 
+- How create wallets
+- Call contracts
 
+# Interact on local node
+
+First, run a local node:
+`npx hardhat node`
+
+Then run
+`npx hardhat interact`
--- Week3/Election/scripts/interact.ts
+++ Week3/Election/scripts/interact.ts
@@ -1,6 +1,17 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { ethers } from 'hardhat';
 
 export const hardhatRun = async (hre: HardhatRuntimeEnvironment) => {
-  console.log(hre,ethers.version);
+  const provider = new hre.ethers.providers.JsonRpcProvider('http://127.0.0.1:8545/');
+  const latestBlock = await provider.getBlock("latest");
+  console.log('Latest block: ' + latestBlock.hash);
+
+  const wallet = new hre.ethers.Wallet(
+    '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
+    provider
+  );
+
+  const balance = await wallet.getBalance();
+  console.log(hre.ethers.utils.formatEther(balance));
 };
 
