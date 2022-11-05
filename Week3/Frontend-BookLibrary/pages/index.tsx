7f58a00dae83857a8929e97cce3be1107e1e9c5c
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2022-11-05 13:48:14 +0200

    W3 - Fix issue with calling contract before provider init

--- Week3/Frontend-BookLibrary/pages/index.tsx
+++ Week3/Frontend-BookLibrary/pages/index.tsx
@@ -1,11 +1,9 @@
 import { useWeb3React } from "@web3-react/core";
 import Head from "next/head";
-import Link from "next/link";
 import Account from "../components/Account";
 import { BookLibrary } from "../components/BookLibrary";
 import NativeCurrencyBalance from "../components/NativeCurrencyBalance";
-import TokenBalance from "../components/TokenBalance";
-import { BOOKLIBRARY_ADDRESS } from "../constants";
+
 import useEagerConnect from "../hooks/useEagerConnect";
 
 function Home() {
@@ -33,7 +31,9 @@ function Home() {
         <h1 className="text-3xl font-bold underline content-center text-center">
           Book Library
         </h1>
-        <BookLibrary />
+        {isConnected && (
+          <BookLibrary />
+        )}
       </main>
     </div>
   );
