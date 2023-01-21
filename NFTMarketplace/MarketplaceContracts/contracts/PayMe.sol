dde52a6a231a81f5c43740ef6004890715460abc
Georgi Ivanov <georgi.ivanovtn@gmail.com>
2023-01-21 09:43:12 +0200

    batch payment

--- NFTMarketplace/MarketplaceContracts/contracts/PayMe.sol
+++ NFTMarketplace/MarketplaceContracts/contracts/PayMe.sol
@@ -3,11 +3,23 @@ pragma solidity 0.8.17;
 
 import '@openzeppelin/contracts/access/Ownable.sol';
 
+import "hardhat/console.sol";
+
 contract PayMe is Ownable {
   event PaymentReceived();
+  event BatchPaymentReceived(uint[] itemIds, uint value);
   event PaymentForItemReceived(uint);
   event BalanceWithdrawn();
   event NonPaymentFunctionCalled();
+  error PaymentAmountNotEnough(uint[] itemids, uint value);
+
+  uint itemPrice;
+
+  constructor() {
+    // The cost of every item price, no matter its id, is 0.0001 ETH
+    itemPrice = 100000000000000;
+  }
+
 
   function nonPayment() public {
     emit NonPaymentFunctionCalled();
@@ -19,6 +31,15 @@ contract PayMe is Ownable {
 
   function payFor(uint itemId) public payable {
     emit PaymentForItemReceived(itemId);
+  }
+
+  function batchPayment(uint[] calldata itemIds) public payable {
+    console.log(itemIds.length);
+    if (itemIds.length * itemPrice < msg.value) {
+      revert PaymentAmountNotEnough(itemIds, msg.value);
+    }
+
+    emit BatchPaymentReceived(itemIds, msg.value);
   }
 
   function withdraw() onlyOwner public {
--- NFTMarketplace/MarketplaceContracts/scripts/payme-interact.ts
+++ NFTMarketplace/MarketplaceContracts/scripts/payme-interact.ts
@@ -5,7 +5,7 @@ import * as helpers from './helpers';
 export async function payMeInteract(ethers: HardhatEthersHelpers) {
   const owner = await helpers.owner(ethers);
   const user1 = await helpers.user1(ethers);
-  console.log(' --- Deploy script ---- ');
+  console.log(' --- Interact script ---- ');
   console.log('Signer ', owner.address);
   
   const payMeFactory = await ethers.getContractFactory('PayMe');
@@ -20,4 +20,10 @@ export async function payMeInteract(ethers: HardhatEthersHelpers) {
   await payMe.withdraw();
 
   console.log('PayMe balance after withdraw:', await payMe.balance());
+
+  console.log('-Batch payment-');
+  tx = await payMe.batchPayment([1, 2, 3], {value: helpers.ethToWei('0.0002')});
+  await tx.wait();
+
+  console.log('Balance after batch payment:', await payMe.balance());
 }
